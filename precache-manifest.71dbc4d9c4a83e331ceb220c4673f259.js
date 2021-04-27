32d44e821459ea006951099402b19a73d542d138
hitesh_kothari <hitesh.kothari@paptronics.com>
2021-04-27 12:46:39 +0530

    Updates

---
+++ precache-manifest.71dbc4d9c4a83e331ceb220c4673f259.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20638c6af523bb6192bd588bd7369f47",
+    "url": "/farm-tech-cart/index.html"
+  },
+  {
+    "revision": "9a95064f1887f2d013ff",
+    "url": "/farm-tech-cart/static/css/2.2d343019.chunk.css"
+  },
+  {
+    "revision": "7b9e5b067cc2a08046ed",
+    "url": "/farm-tech-cart/static/css/main.6aad2db4.chunk.css"
+  },
+  {
+    "revision": "9a95064f1887f2d013ff",
+    "url": "/farm-tech-cart/static/js/2.d5722d38.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/farm-tech-cart/static/js/2.d5722d38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b9e5b067cc2a08046ed",
+    "url": "/farm-tech-cart/static/js/main.427ba72e.chunk.js"
+  },
+  {
+    "revision": "b96e5ac0eb20ed12b86b",
+    "url": "/farm-tech-cart/static/js/runtime-main.ce11a2cf.js"
+  },
+  {
+    "revision": "fa0d271853ce35dab81ca2360aec570c",
+    "url": "/farm-tech-cart/static/media/shopping-cart-solid.fa0d2718.svg"
+  }
+]);
